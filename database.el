c592a9ea8dfb04c1f8af7239c85aaf95a291ae45
Cameron Cooper <zdot101@gmail.com>
2019-08-17 22:25:25 -0400

    Added our primitive (db-exec) for calling mysql commands (and later other commands) ,  the app will naturally be wrapped around this entirely

--- database.el
+++ database.el
@@ -10,3 +10,30 @@
                (dbm/new-database "root" "password" "obsidian-chat")))
 (defvar +database+
   (dbm/new-database "root" "password" "obsidian-chat"))
+(defun dbm/db-exec (db command)
+  (shell-command-to-string
+   (concat
+    "mysql "
+    "-u" (alist-get :username db) " "
+    "-p" (alist-get :password db) " "
+    (alist-get :database db) " "
+    "-e"
+    "'" command "'")))
+(assert (s-match "user_id" (dbm/db-exec test-database "show columns from messages")))
+(assert (s-match "channel_id" (dbm/db-exec test-database "show columns from messages")))
+(assert (not (s-match "ass_id" (dbm/db-exec test-database "show columns from messages"))))
+
+(defun dbm/raw-show-db-columns (db table)
+  (dbm/db-exec db (concat "show columns from " table)))
+(assert (s-match "user_id" (dbm/raw-show-db-columns test-database "messages")))
+(assert (s-match "channel_id" (dbm/raw-show-db-columns test-database "messages")))
+(assert (not (s-match "ass_id" (dbm/raw-show-db-columns test-database "messages"))))
+
+(defun dbm/get-db-columns (db table)
+  (->>
+   (split-string (dbm/raw-show-db-columns db table) "\n")
+   rest
+   rest
+   (mapcar (lambda (x) (-> x split-string car)))
+   (-drop-last 1)))
+(assert (equal '("id" "user_id" "channel_id" "message" "created_at") (dbm/get-db-columns test-database "messages")))
